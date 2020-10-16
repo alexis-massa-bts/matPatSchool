a3284cdcacf3a2f380db1d43f65197d11e705d06
Alexis Massa <63648046+massa-alexis@users.noreply.github.com>
2020-10-16 16:06:37 +0200

    deleted duplicata of models again

--- matpat-lms/src/backend/route/lesson_route.js
+++ matpat-lms/src/backend/route/lesson_route.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const lessonController = require("../controller/lesson_controller");
 
-router.get('/lesson', lessonController.getLessons);
+// router.get('/lesson', lessonController.getLessons);
+router.route("/").get((req, res) => {
+    res.send('oui');
+});
 
 router.post('lesson/add', lessonController.createlesson);
 
