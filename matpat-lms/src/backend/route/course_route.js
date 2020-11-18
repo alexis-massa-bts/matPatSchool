ca9ff86f1b583b788329730f2cb460e5d71c7ea7
iloxe <fifoualex73@gmail.com>
2020-11-18 20:18:28 +0100

    restart lesson

--- matpat-lms/src/backend/route/course_route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const courseController = require("../controller/course_controller");
-
-router.get('/course', courseController.getcourses);
-
-router.post('course/add', courseController.createcourse);
-
-module.exports = router;
