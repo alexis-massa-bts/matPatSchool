d15d7d968cb5371a93c4f12912cba65fd2bfafc4
Alexis Massa <63648046+massa-alexis@users.noreply.github.com>
2020-11-20 17:42:48 +0100

    gfdqs

--- matpat-lms/src/backend/route/lesson.route.js
+++ matpat-lms/src/backend/route/lesson.route.js
@@ -4,7 +4,18 @@ const db = require("../db_connection");
 const collection = "Lesson";
 
 //CREATE
-
+router.route("/add").post((req, res) => {
+  const userInput = req.body;
+  db.getDB()
+    .collection(collection)
+    .insertOne(userInput, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json({ result: result, document: result.ops[0] });
+      }
+    });
+});
 
 //READ
 router.route("/").get((req, res) => {
@@ -25,18 +36,34 @@ router.route("/").get((req, res) => {
 router.route("/:id").put((req, res) => {
   const lessonID = req.params.id;
   const userInput = req.body;
-  db.getDB().collection(collection).findOneAndUpdate(
-    { _id: db.getPrimaryKey(lessonID) },
-    { $set: { lessonName: userInput.lessonName} },
-    { returnOriginal: false },
-    (err, result) => {
+  db.getDB()
+    .collection(collection)
+    .findOneAndUpdate(
+      { _id: db.getPrimaryKey(lessonID)},
+      { $set: { lessonName: userInput.lessonName } },
+      { returnOriginal: false },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(result);
+        }
+      }
+    );
+});
+
+//DELETE
+router.route("/:id").delete((req, res) => {
+  const lessonID = req.params.id;
+  db.getDB()
+    .collection(collection)
+    .findOneAndDelete({ _id: db.getPrimaryKey(lessonID) }, (err, result) => {
       if (err) {
         console.log(err);
       } else {
         res.json(result);
       }
-    }
-  );
+    });
 });
 
 // router.post('/add', function (req, res) {
